230c74376786293059ef47290aa81fa1312572e9
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2025-02-11 00:05:32 +0700

    Refactor code

--- Prime.cpp
+++ Prime.cpp
@@ -14,56 +14,82 @@
 
 struct PerSocketData {};
 
-static void HandleClientConnection(uWS::WebSocket<false, true, PerSocketData>* ws, std::atomic<bool>& running, std::atomic<int>& frame) {
+std::atomic<bool> running = false;
+std::atomic<int> frame = 0;
+std::jthread frameCaptureThread;
+std::jthread fpsThread;
+
+static void FrameCaptureThread(uWS::WebSocket<false, true, PerSocketData>* ws) {
+    ScreenshotService screenshotService;
+    std::vector<unsigned char> frameBuffer;
+
+    while (running) {
+        if (!screenshotService.GetNextFrame(frameBuffer)) {
+            continue;
+        }
+
+        std::vector<unsigned char> jpgBuffer;
+        if (!ImageConverter::ToJPG(frameBuffer, jpgBuffer)) {
+            continue;
+        }
+
+        std::string_view jpeg_view(reinterpret_cast<const char*>(jpgBuffer.data()), jpgBuffer.size());
+        ws->send(jpeg_view, uWS::BINARY);
+
+        frame++;
+    }
+}
+
+static void FPSThread() {
+    while (running) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "FPS: " << frame << std::endl;
+        frame = 0;
+    }
+}
+
+static void HandleClientConnection(uWS::WebSocket<false, true, PerSocketData>* ws) {
     std::cout << "Client connected" << std::endl;
 
     running = true;
 
-    std::jthread([&ws, &running, &frame]() {
-        ScreenshotService screenshotService;
-        std::vector<unsigned char> frameBuffer;
+    if (!frameCaptureThread.joinable()) {
+        frameCaptureThread = std::jthread(FrameCaptureThread, ws);
+    }
 
-        while (running) {
-            if (!screenshotService.GetNextFrame(frameBuffer)) {
-                continue;
-            }
+    if (!fpsThread.joinable()) {
+        fpsThread = std::jthread(FPSThread);
+    }
+}
 
-            std::vector<unsigned char> jpgBuffer;
-            if (!ImageConverter::ToJPG(frameBuffer, jpgBuffer)) {
-                continue;
-            }
+static void HandleClientDisconnection(uWS::App* app) {
+    std::cout << "Client disconnected" << std::endl;
+    running = false;
 
-            std::string_view jpeg_view(reinterpret_cast<const char*>(jpgBuffer.data()), jpgBuffer.size());
-            ws->send(jpeg_view, uWS::BINARY);
+    if (frameCaptureThread.joinable()) {
+        frameCaptureThread.join();
+    }
+    if (fpsThread.joinable()) {
+        fpsThread.join();
+    }
 
-            frame++;
-        }
-    }).detach();
-
-    std::jthread([&ws, &running, &frame]() {
-        while (running) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            std::cout << "FPS: " << frame << std::endl;
-            frame = 0;
-        }
-    }).detach();
+    app->close();
 }
 
 int main() {
-    std::atomic<bool> running = false;
-    std::atomic<int> frame = 0;
+    uWS::App app;
 
-    uWS::App().ws<PerSocketData>("/*", {
+    app.ws<PerSocketData>("/*", {
         .compression = uWS::SHARED_COMPRESSOR,
         .maxPayloadLength = 16 * 1024 * 1024, // 16MB
         .idleTimeout = 10, // 10 seconds
         .maxBackpressure = 1 * 1024 * 1024, // 1MB
-        .open = [&running, &frame]([[maybe_unused]] auto* ws) {
-            HandleClientConnection(ws, running, frame);
+        .open = []([[maybe_unused]] auto* ws) {
+            HandleClientConnection(ws);
         },
-        .close = []([[maybe_unused]] auto* ws, [[maybe_unused]] int code, [[maybe_unused]] std::string_view message) {
-            std::cout << "Client disconnected" << std::endl;
-        }
+        .close = [&app]([[maybe_unused]] auto* ws, [[maybe_unused]] int code, [[maybe_unused]] std::string_view message) {
+            HandleClientDisconnection(&app);
+        },
     }).listen(8080, [](auto* token) {
         if (token) {
             std::cout << "Server started at ws://localhost:8080" << std::endl;
